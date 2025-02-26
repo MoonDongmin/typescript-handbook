fc1af3ea7eb3c51ed5a10c66c6fa1e5579bea569
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-02-26 13:21:03 +0900

    4. 타입 별칭과 인덱스 시그니처

---
+++ section02.TypeScript.basics/chapter02/dist/chapter03/chapter03.js
@@ -0,0 +1,15 @@
+// let user: object = {
+//     id: 1,
+//     name: "문동민",
+// };
+//
+// user.id;
+let user = {
+    id: 1,
+    name: "문동민",
+};
+user = {
+    name: "홍길동",
+};
+user.name = 'asfd'; // 오류 발생
+export {};
---
+++ section02.TypeScript.basics/chapter02/src/chapter04/chapter04.ts
@@ -0,0 +1,43 @@
+type User = {
+    id: number;
+    name: string;
+    nickname: string;
+    birth: string;
+    bio: string;
+    location: string;
+};
+
+let user: User = {
+    id: 1,
+    name: "이정환",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시",
+};
+
+let user2: User = {
+    id: 2,
+    name: "홍길동",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시",
+};
+
+function test() {
+    type User = string; // 무방함
+}
+
+// 인덱스 시그니처(Index Signature)
+type CountryCodes = {
+    [key: string]: string;
+};
+
+let countryCodes: CountryCodes = {
+    Korea: "ko",
+    UnitedState: "us",
+    UnitedKingdom: "uk",
+    // (... 약 100개의 국가)
+    Brazil : 'bz'
+};
